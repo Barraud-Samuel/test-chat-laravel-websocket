c6841f527eec0a45c1b5377475696bc2bc6db214
barraud-samuel <samuel.barraud@hetic.net>
2020-09-03 13:30:08 +0200

    try to fix stuff

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -39,7 +39,7 @@ window.Echo = new Echo({
     key: process.env.MIX_PUSHER_APP_KEY,
     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
     wsHost: process.env.MIX_PUSHER_HOST,
-    wsPort: 6002,
+    wsPort: 6001,
     forceTLS: false,
     disableStats: true,
 });
