342efb7f5822dcfe35b9f1ac1d6f38f8fcd05d14
barraud-samuel <samuel.barraud@hetic.net>
2021-01-27 16:37:44 +0100

    fix https link

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        if($this->app->environment('production'))
+        {
+            $this->app['request']->server->set('HTTPS','on');
+        }
     }
 
     /**
